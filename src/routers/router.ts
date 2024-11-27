4e1c9e830f7e4f55e6d340ed738e89b7bf015512
Riquelme <riquelmestayler@gmail.com>
2024-11-27 00:36:23 -0300

    finalized all routes 2.0

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -5,16 +5,22 @@ import { midleware } from '../midleware/jwt'
 
 export const router = Router()
 
+// Authentication
 router.post('/auth/signup', userController.registerUser)
 router.post('/auth/signin', userController.loginUser)
 
+//Posts
+router.get('/posts', midleware, postController.getPosts)
 router.get('/post/:name', midleware, postController.getPostsUser)
-router.post('/post', midleware, postController.newPost)
 router.get('/category/posts', midleware, postController.getPostsCategory)
+router.post('/post', midleware, postController.newPost)
 router.patch('/post/:id', midleware, postController.updatePost)
 router.delete('/post/:id', midleware, postController.deletePost)
-router.get('/posts', midleware, postController.getPosts)
+
+//Likes
 router.post('/post/like/:id', midleware, postController.toogleLikePost)
+
+//Comments
 router.post('/post/comment/:id', midleware, postController.addComment)
 router.delete('/post/comment/:id', midleware, postController.removeComment)
 router.patch('/post/comment/:id', midleware, postController.updateComment)
